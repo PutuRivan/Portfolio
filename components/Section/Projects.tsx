9144e586e15b4d875349bf65ea81acf3625eda3b
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2024-06-28 20:47:53 +0700

    commit

--- components/Section/Projects.tsx
+++ components/Section/Projects.tsx
@@ -5,7 +5,7 @@ import { Project } from "../Data/Project";
 import ProjectDataProvider from "../Contents/ProjectDataProvider";
 const Projects = () => {
   return (
-    <section id="projects" className="w-full h-full mt-40 mb-10 pt-20">
+    <section id="projects" className="w-full h-full mt-40 mb-10">
       <div className="w-full h-full flex flex-col items-center justify-center pt-20">
         <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-blue-500 to-cyan-500">
           Projects
--- components/Section/Skills.tsx
+++ components/Section/Skills.tsx
@@ -4,13 +4,13 @@ import SkillDataProvider from "../Contents/SkillDataProviders";
 
 const Skills = () => {
   return (
-    <section id="skills" className="w-full h-full mt-40 px-10 pt-40">
+    <section id="skills" className="w-full h-full mt-40 px-10">
       <div className="w-full h-full flex flex-col items-center justify-center gap-20">
         <h1 className="text-4xl text-transparent bg-clip-text bg-gradient-to-r from-cyan-500 to-blue-500">
           Skills
         </h1>
         <div className="w-auto h-auto">
-          <div className="flex flex-row justify-around flex-wrap mt-4 gap-20 items-center">
+          <div className="flex flex-row justify-around flex-wrap mt-4 gap-10 items-center">
             {Skill_data.map((image, index) => (
               <SkillDataProvider
                 key={index}
