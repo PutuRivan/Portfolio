f7d9fac1b543e7e84337e70a11fcd253d9b7a6f0
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2024-06-28 18:13:28 +0700

    fix:typo at contact

--- components/Section/Contact.tsx
+++ components/Section/Contact.tsx
@@ -19,7 +19,7 @@ const Contact = () => {
         className="flex flex-col gap-5 bg-gray-800 p-5 rounded-xl w-[400px]"
       >
         <div className="flex flex-col">
-          <h3 className="text-lg">Let&apos;s now each other</h3>
+          <h3 className="text-lg">Let&apos;s know each other</h3>
         </div>
         <div className="flex flex-col justify-center items-center gap-2">
           <a href="" className="btn btn-outline btn-primary z-10">
