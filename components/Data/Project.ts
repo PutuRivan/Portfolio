ff33997206eb9300c6b59cd62124cf4e26e1cf21
PutuRivan <150578221+PutuRivan@users.noreply.github.com>
2025-01-03 13:58:59 +0700

    feat:add the buncitman project to list

--- components/Data/Project.ts
+++ components/Data/Project.ts
@@ -4,21 +4,28 @@ export const Project = [
       name: "Manga List",
       link: "https://mangalist.vercel.app/",
       img: "Project/manga-list-1.png",
-      description:"Manga list project with React, Next.js, Tailwind CSS and Vercel. Using API from mangadex.org"
+      description:"Manga list project with React, Next.js, Tailwind CSS and Vercel. Using API from jikan api"
     },
     {
       id: 2,
       name: "Anime List",
       link: "https://indonesia-anime.vercel.app/",
       img: "Project/anime-list-1.png",
-      description:"Anime list project with React, Next.js, Tailwind CSS and Vercel. Using API from anilist.co"
+      description:"Anime list project with React, Next.js, Tailwind CSS and Vercel. Using API from jikan api"
     },
     {
       id: 3,
       name: "Savory",
       link: "https://restaurant-savory.vercel.app/",
       img: "Project/Savory-1.png",
-      description:"Savory project with React, Next.js, Tailwind CSS and Vercel. Using API from Zomato"
+      description:"Savory project with React, Next.js, Tailwind CSS and Vercel"
+    },
+    {
+      id: 4,
+      name: "The buncitman",
+      link: "https://the-buncitman.vercel.app/",
+      img: "Project/The-Buncitman-1.png",
+      description:"The Buncitman project with Next.js, Tailwind CSS, Prisma, PostgreSQL, NextAuth and Vercel. Using API from Zomato"
     },
   ];
   
